1237a2ae1f49b6f105fae064eba0dc4f29d174a0
btnguyen2k <btnguyen2k@gmail.com>
2008-04-18 09:13:16 +0000

    * Changed package name Xconfig to Xpath * Created template file for "AllTest" bootstrap

--- src/phpunit/Mls/php5/AllTests.php
+++ src/phpunit/Mls/php5/AllTests.php
@@ -17,57 +17,8 @@
  * @since      	File available since v0.1
  */
 
-//initialization
-if ( !defined('PHPUnit_MAIN_METHOD') ) {
-    define('PHPUnit_MAIN_METHOD', 'AllTests::main');
-}
-
-//defines package name and package php version
 define('PACKAGE', 'Mls');
-define('PACKAGE_PHP_VERSION', 'php5');
 $REQUIRED_PACKAGES = Array('Commons');
 
-//setting up include path
-$dir = dirname(dirname(dirname(dirname(__FILE__))));
-$INCLUDE_PATH = '.';
-$INCLUDE_PATH .= PATH_SEPARATOR.$dir.'/libs/PHPUnit-3.2.9';
-$INCLUDE_PATH .= PATH_SEPARATOR.$dir.'/'.PACKAGE.'/'.PACKAGE_PHP_VERSION;
-foreach ( $REQUIRED_PACKAGES as $package ) {
-    $INCLUDE_PATH .= PATH_SEPARATOR.$dir.'/'.$package.'/'.PACKAGE_PHP_VERSION;
-}
-ini_set('include_path', $INCLUDE_PATH);
-
-if ( count($argv) > 1 ) {
-    $logFile = $argv[1];
-    @unlink($logFile);
-    ini_set('error_log', $logFile);
-}
-
-require_once 'PHPUnit/Framework.php';
-require_once 'PHPUnit/TextUI/TestRunner.php';
-
-class AllTests {
-    public static function main() {
-        PHPUnit_TextUI_TestRunner::run(self::suite());
-    }
-
-    public static function suite() {
-        $suite = new PHPUnit_Framework_TestSuite('PHPUnit');
-        if ( $handle = opendir('.') ) {
-            while ( false !== ($file = readdir($handle)) ) {
-                if ( $file != "AllTests.php" ) {
-                    if ( preg_match("/^([\\w]+Test)\\.php$/", $file, $matches) ) {
-                        include_once $file;
-                        $suite->addTestSuite($matches[1]);
-                    }
-                }
-            }
-        }
-        return $suite;
-    }
-}
-
-if ( PHPUnit_MAIN_METHOD == 'AllTests::main' ) {
-    AllTests::main();
-}
+require '../../php5/Template_AllTest.php';
 ?>
