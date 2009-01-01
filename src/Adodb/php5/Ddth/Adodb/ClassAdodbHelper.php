adc11eb918f7875d90190e2b0678c77a4995fb2f
btnguyen2k <btnguyen2k@gmail.com>
2009-01-01 12:04:52 +0000

    * Adodb v0.1.5.1: Added function Ddth_Adodb_AdodbHelper::prepareSql()

--- src/Adodb/php5/Ddth/Adodb/ClassAdodbHelper.php
+++ src/Adodb/php5/Ddth/Adodb/ClassAdodbHelper.php
@@ -29,20 +29,39 @@
  * @since      	Class available since v0.1.2
  */
 class Ddth_Adodb_AdodbHelper {
+
     /**
      * A shortcut to to build string with n question marks separated by commas (e.g. "?,?,?,?").
      * 
      * @param int
      * @return string
      */
-    public static function buildArrayParams($count=1) {
+    public static function buildArrayParams($count = 1) {
         $count += 0;
-        if ( $count < 1 ) return '';
+        if ( $count < 1 ) {
+            return '';
+        }
         $result = '?';
         for ( $i = 1; $i < $count; $i++ ) {
             $result .= ',?';
         }
         return $result;
+    }
+
+    /**
+     * Prepares the SQL statement.
+     * 
+     * @param ADOConnection an active ADOdb connection
+     * @param string the SQL statement
+     * @param Array list of named binding parameters (case-sensitive!)
+     * @return string the prepared SQL statement
+     * @since function available since v0.1.5.1
+     */
+    public static function prepareSql($conn, $sql, $params = Array()) {
+        foreach ( $params as $p ) {
+            $sql = str_replace('${' . $p . '}', $conn->Param($p), $sql);
+        }
+        return $sql;
     }
 }
 ?>
