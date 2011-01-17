b881244130fbc2cb95b2978067fa10cd486bb4b9
btnguyen2k@gmail.com <btnguyen2k@gmail.com@0bc26c09-3644-0410-ad72-b9ecc7d9cb2a>
2011-01-17 09:48:58 +0000

    * Released Adodb v0.2

--- trunk/src/phpunit/Adodb/php5/AdodbSqlStatementTest.php
+++ trunk/src/phpunit/Adodb/php5/AdodbSqlStatementTest.php
@@ -21,94 +21,71 @@ class AdodbSqlStatementTest extends PHPUnit_Framework_TestCase {
     protected function setup() {
         parent::setUp();
         $dir = '../../../tmp';
-        if ( !is_dir($dir) ) {
+        if (!is_dir($dir)) {
             mkdir($dir);
         }
         @unlink('../../../tmp/adodbtest.db');
-        //copy('../../../firebirdembed/blankdb.gdb', '../../../tmp/adodbtest.gdb');
-
         global $DPHP_ADODB_CONFIG;
-        $DPHP_ADODB_CONFIG = Array(
-            'adodb.driver'      => 'sqlite'
-            ,
-            'adodb.host'        => '../../../tmp/adodbtest.db'
-            ,
-            'adodb.user'        => ''
-            ,
-            'adodb.password'    => ''
-            ,
-            'adodb.database'    => 'adodbtest'
-            ,
-            #'adodb.url'         => 'mysql://test:test@localhost/test'
-            #,
-            #'adodb.setupSqls    => "SET NAMES 'utf8'"
-            );
+        $DPHP_ADODB_CONFIG = Array('adodb.driver' => 'sqlite',
+                'adodb.host' => '../../../tmp/adodbtest.db',
+                'adodb.user' => '',
+                'adodb.password' => '',
+                'adodb.database' => 'adodbtest');
     }
 
     /**
      * Tests creation of Ddth_Adodb_AdodbSqlStatementFactory objects.
      */
     public function testObjCreation() {
-        $obj1 = Ddth_Adodb_AdodbSqlStatementFactory::getInstance(self::CONFIG_FILE);
-        $this->assertNotNull($obj1, "Can not create Ddth_Adodb_AdodbSqlStatementFactory object!");
+        $obj1 = Ddth_Adodb_AdodbHelper::loadSqlStatement(self::CONFIG_FILE);
+        $this->assertNotNull($obj1, "Can not load SQL statements!");
+        $this->assertTrue($obj1 instanceof Ddth_Commons_Properties, "The returned object is expected to be an instance of Ddth_Commons_Properties!");
 
-        $obj2 = Ddth_Adodb_AdodbSqlStatementFactory::getInstance(self::CONFIG_FILE);
-        $this->assertTrue($obj1===$obj2, "The two objects are expected to be equal!");
+        $obj2 = Ddth_Adodb_AdodbHelper::loadSqlStatement(self::CONFIG_FILE);
+        $this->assertTrue($obj1 === $obj2, "The two objects are expected to be equal!");
     }
 
     public function testGetStatement() {
-        $factory = Ddth_Adodb_AdodbSqlStatementFactory::getInstance(self::CONFIG_FILE);
-        $this->assertNotNull($factory, "Can not create Ddth_Adodb_AdodbSqlStatementFactory object!");
-
         $name = 'sql.createTable';
-        $stm = $factory->getSqlStatement($name);
+        $stm = Ddth_Adodb_AdodbHelper::loadSqlStatement(self::CONFIG_FILE, $name);
         $this->assertNotNull($stm, "Can not get statement [$name]!");
-        $this->assertTrue($stm instanceof Ddth_Adodb_AdodbSqlStatement);
 
         $name = 'sql.createUser';
-        $stm = $factory->getSqlStatement($name);
+        $stm = Ddth_Adodb_AdodbHelper::loadSqlStatement(self::CONFIG_FILE, $name);
         $this->assertNotNull($stm, "Can not get statement [$name]!");
-        $this->assertTrue($stm instanceof Ddth_Adodb_AdodbSqlStatement);
 
         $name = 'sql.getUserById';
-        $stm = $factory->getSqlStatement($name);
+        $stm = Ddth_Adodb_AdodbHelper::loadSqlStatement(self::CONFIG_FILE, $name);
         $this->assertNotNull($stm, "Can not get statement [$name]!");
-        $this->assertTrue($stm instanceof Ddth_Adodb_AdodbSqlStatement);
 
         $name = 'sql.getUserByUsername';
-        $stm = $factory->getSqlStatement($name);
+        $stm = Ddth_Adodb_AdodbHelper::loadSqlStatement(self::CONFIG_FILE, $name);
         $this->assertNotNull($stm, "Can not get statement [$name]!");
-        $this->assertTrue($stm instanceof Ddth_Adodb_AdodbSqlStatement);
 
         $name = 'sql.getUserByEmail';
-        $stm = $factory->getSqlStatement($name);
+        $stm = Ddth_Adodb_AdodbHelper::loadSqlStatement(self::CONFIG_FILE, $name);
         $this->assertNotNull($stm, "Can not get statement [$name]!");
-        $this->assertTrue($stm instanceof Ddth_Adodb_AdodbSqlStatement);
 
         $name = 'sql.not-exist';
-        $stm = $factory->getSqlStatement($name);
+        $stm = Ddth_Adodb_AdodbHelper::loadSqlStatement(self::CONFIG_FILE, $name);
         $this->assertNull($stm);
     }
 
     public function testQueryCreateTable() {
-        $stmFactory = Ddth_Adodb_AdodbSqlStatementFactory::getInstance(self::CONFIG_FILE);
-        $this->assertNotNull($stmFactory, "Can not create Ddth_Adodb_AdodbSqlStatementFactory object!");
-
         $connFactory = Ddth_Adodb_AdodbFactory::getInstance();
 
         $exception = NULL;
         $conn = $connFactory->getConnection();
-        try{
+        try {
             $name = 'sql.createTable';
-            $stm = $stmFactory->getSqlStatement($name);
+            $stm = Ddth_Adodb_AdodbHelper::loadSqlStatement(self::CONFIG_FILE, $name);
             $this->assertNotNull($stm, "Can not get statement [$name]!");
-            $this->assertTrue($stm instanceof Ddth_Adodb_AdodbSqlStatement);
-            $stm->prepareAndExecute($conn);
-        } catch ( Exception $e ) {
+            $conn->Execute($stm);
+        } catch (Exception $e) {
             $exception = $e;
         }
         $connFactory->closeConnection($conn);
-        if ( $exception !== NULL ) {
+        if ($exception !== NULL) {
             throw $exception;
         }
     }
@@ -116,35 +93,28 @@ class AdodbSqlStatementTest extends PHPUnit_Framework_TestCase {
     public function testQueryCreateUsers() {
         $this->testQueryCreateTable();
 
-        $stmFactory = Ddth_Adodb_AdodbSqlStatementFactory::getInstance(self::CONFIG_FILE);
-        $this->assertNotNull($stmFactory, "Can not create Ddth_Adodb_AdodbSqlStatementFactory object!");
-
         $connFactory = Ddth_Adodb_AdodbFactory::getInstance();
 
         $exception = NULL;
         $conn = $connFactory->getConnection(TRUE);
-        try{
+        try {
             $name = 'sql.createUser';
-            $stm = $stmFactory->getSqlStatement($name);
+            $stm = Ddth_Adodb_AdodbHelper::loadSqlStatement(self::CONFIG_FILE, $name);
             $this->assertNotNull($stm, "Can not get statement [$name]!");
-            $this->assertTrue($stm instanceof Ddth_Adodb_AdodbSqlStatement);
-            $stm->prepareAndExecute($conn, Array(1, 'first',  'first-user@local'));
-            $stm->prepareAndExecute($conn, Array(2, 'second', 'second-user@local'));
-            $stm->prepareAndExecute($conn, Array(3, 'third',  'third-user@local'));
-        } catch ( Exception $e ) {
+            $conn->Execute($stm, Array(1, 'first', 'first-user@local'));
+            $conn->Execute($stm, Array(2, 'second', 'second-user@local'));
+            $conn->Execute($stm, Array(3, 'third', 'third-user@local'));
+        } catch (Exception $e) {
             $exception = $e;
         }
         $connFactory->closeConnection($conn);
-        if ( $exception !== NULL ) {
+        if ($exception !== NULL) {
             throw $exception;
         }
     }
 
     public function testQueryGetUser1() {
         $this->testQueryCreateUsers();
-
-        $stmFactory = Ddth_Adodb_AdodbSqlStatementFactory::getInstance(self::CONFIG_FILE);
-        $this->assertNotNull($stmFactory, "Can not create Ddth_Adodb_AdodbSqlStatementFactory object!");
 
         $connFactory = Ddth_Adodb_AdodbFactory::getInstance();
 
@@ -153,30 +123,26 @@ class AdodbSqlStatementTest extends PHPUnit_Framework_TestCase {
         $conn->SetFetchMode(ADODB_FETCH_ASSOC);
         try {
             $name = 'sql.getUserById';
-            $stm = $stmFactory->getSqlStatement($name);
+            $stm = Ddth_Adodb_AdodbHelper::loadSqlStatement(self::CONFIG_FILE, $name);
             $this->assertNotNull($stm, "Can not get statement [$name]!");
-            $this->assertTrue($stm instanceof Ddth_Adodb_AdodbSqlStatement);
-            $resultSet = $stm->prepareAndExecute($conn, Array(1));
+            $resultSet = $conn->Execute($stm, Array(1));
             $this->assertNotNull($resultSet);
             $this->assertFalse($resultSet->EOF);
             $this->assertEquals(1, $resultSet->fields['id']);
             $this->assertEquals('first', $resultSet->fields['username']);
             $this->assertEquals('first-user@local', $resultSet->fields['email']);
             $resultSet->Close();
-        } catch ( Exception $e ) {
+        } catch (Exception $e) {
             $exception = $e;
         }
         $connFactory->closeConnection($conn);
-        if ( $exception !== NULL ) {
+        if ($exception !== NULL) {
             throw $exception;
         }
     }
 
     public function testQueryGetUser2() {
         $this->testQueryCreateUsers();
-
-        $stmFactory = Ddth_Adodb_AdodbSqlStatementFactory::getInstance(self::CONFIG_FILE);
-        $this->assertNotNull($stmFactory, "Can not create Ddth_Adodb_AdodbSqlStatementFactory object!");
 
         $connFactory = Ddth_Adodb_AdodbFactory::getInstance();
 
@@ -185,30 +151,26 @@ class AdodbSqlStatementTest extends PHPUnit_Framework_TestCase {
         $conn->SetFetchMode(ADODB_FETCH_ASSOC);
         try {
             $name = 'sql.getUserByUsername';
-            $stm = $stmFactory->getSqlStatement($name);
+            $stm = Ddth_Adodb_AdodbHelper::loadSqlStatement(self::CONFIG_FILE, $name);
             $this->assertNotNull($stm, "Can not get statement [$name]!");
-            $this->assertTrue($stm instanceof Ddth_Adodb_AdodbSqlStatement);
-            $resultSet = $stm->prepareAndExecute($conn, Array('second'));
+            $resultSet = $conn->Execute($stm, Array('second'));
             $this->assertNotNull($resultSet);
             $this->assertFalse($resultSet->EOF);
             $this->assertEquals(2, $resultSet->fields['id']);
             $this->assertEquals('second', $resultSet->fields['username']);
             $this->assertEquals('second-user@local', $resultSet->fields['email']);
             $resultSet->Close();
-        } catch ( Exception $e ) {
+        } catch (Exception $e) {
             $exception = $e;
         }
         $connFactory->closeConnection($conn);
-        if ( $exception !== NULL ) {
+        if ($exception !== NULL) {
             throw $exception;
         }
     }
 
     public function testQueryGetUser3() {
         $this->testQueryCreateUsers();
-
-        $stmFactory = Ddth_Adodb_AdodbSqlStatementFactory::getInstance(self::CONFIG_FILE);
-        $this->assertNotNull($stmFactory, "Can not create Ddth_Adodb_AdodbSqlStatementFactory object!");
 
         $connFactory = Ddth_Adodb_AdodbFactory::getInstance();
 
@@ -217,30 +179,26 @@ class AdodbSqlStatementTest extends PHPUnit_Framework_TestCase {
         $conn->SetFetchMode(ADODB_FETCH_ASSOC);
         try {
             $name = 'sql.getUserByEmail';
-            $stm = $stmFactory->getSqlStatement($name);
+            $stm = Ddth_Adodb_AdodbHelper::loadSqlStatement(self::CONFIG_FILE, $name);
             $this->assertNotNull($stm, "Can not get statement [$name]!");
-            $this->assertTrue($stm instanceof Ddth_Adodb_AdodbSqlStatement);
-            $resultSet = $stm->prepareAndExecute($conn, Array('third-user@local'));
+            $resultSet = $conn->Execute($stm, Array('third-user@local'));
             $this->assertNotNull($resultSet);
             $this->assertFalse($resultSet->EOF);
             $this->assertEquals(3, $resultSet->fields['id']);
             $this->assertEquals('third', $resultSet->fields['username']);
             $this->assertEquals('third-user@local', $resultSet->fields['email']);
             $resultSet->Close();
-        } catch ( Exception $e ) {
+        } catch (Exception $e) {
             $exception = $e;
         }
         $connFactory->closeConnection($conn);
-        if ( $exception !== NULL ) {
+        if ($exception !== NULL) {
             throw $exception;
         }
     }
 
     public function testQueryGetUser4() {
         $this->testQueryCreateUsers();
-
-        $stmFactory = Ddth_Adodb_AdodbSqlStatementFactory::getInstance(self::CONFIG_FILE);
-        $this->assertNotNull($stmFactory, "Can not create Ddth_Adodb_AdodbSqlStatementFactory object!");
 
         $connFactory = Ddth_Adodb_AdodbFactory::getInstance();
 
@@ -249,18 +207,48 @@ class AdodbSqlStatementTest extends PHPUnit_Framework_TestCase {
         $conn->SetFetchMode(ADODB_FETCH_ASSOC);
         try {
             $name = 'sql.getUserById';
-            $stm = $stmFactory->getSqlStatement($name);
+            $stm = Ddth_Adodb_AdodbHelper::loadSqlStatement(self::CONFIG_FILE, $name);
             $this->assertNotNull($stm, "Can not get statement [$name]!");
-            $this->assertTrue($stm instanceof Ddth_Adodb_AdodbSqlStatement);
-            $resultSet = $stm->prepareAndExecute($conn, Array(0));
+            $resultSet = $conn->Execute($stm, Array(0));
             $this->assertNotNull($resultSet);
             $this->assertTrue($resultSet->EOF);
             $resultSet->Close();
-        } catch ( Exception $e ) {
+        } catch (Exception $e) {
             $exception = $e;
         }
         $connFactory->closeConnection($conn);
-        if ( $exception !== NULL ) {
+        if ($exception !== NULL) {
+            throw $exception;
+        }
+    }
+
+    public function testQueryMultiParams1() {
+        $this->testQueryCreateUsers();
+
+        $connFactory = Ddth_Adodb_AdodbFactory::getInstance();
+
+        $exception = NULL;
+        $conn = $connFactory->getConnection();
+        $conn->SetFetchMode(ADODB_FETCH_ASSOC);
+        try {
+            $name = 'sql.multiParams1';
+            $stm = Ddth_Adodb_AdodbHelper::loadSqlStatement(self::CONFIG_FILE, $name);
+            $this->assertNotNull($stm, "Can not get statement [$name]!");
+            $resultSet = $conn->Execute($stm, Array(1, 'first-user@local', 2));
+            $this->assertNotNull($resultSet);
+            $this->assertFalse($resultSet->EOF);
+            $count = 0;
+            while (!$resultSet->EOF) {
+                $count++;
+                $resultSet->MoveNext();
+            }
+            $this->assertEquals(2, $count);
+            $resultSet->Close();
+        } catch (Exception $e) {
+            $exception = $e;
+        }
+        $connFactory->closeConnection($conn);
+        if ($exception !== NULL) {
             throw $exception;
         }
     }
