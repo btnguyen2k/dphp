39697c56d1e6ebb540032d899e9cc73e1d00b5a9
btnguyen2k <btnguyen2k@gmail.com>
2011-01-17 09:48:58 +0000

    * Released Adodb v0.2

--- src/Adodb/php5/Ddth/Adodb/ClassAdodbHelper.php
+++ src/Adodb/php5/Ddth/Adodb/ClassAdodbHelper.php
@@ -7,12 +7,10 @@
  *
  * COPYRIGHT: See the included copyright.txt file for detail.
  *
- * @package		Adodb
- * @author		Thanh Ba Nguyen <btnguyen2k@gmail.com>
- * @copyright	2008 DDTH.ORG
- * @license    	http://www.gnu.org/licenses/lgpl.html  LGPL 3.0
- * @version			$Id$
- * @since      	File available since v0.1.2
+ * @package     Adodb
+ * @author      Thanh Ba Nguyen <btnguyen2k@gmail.com>
+ * @version     $Id$
+ * @since       File available since v0.1.2
  */
 
 /**
@@ -25,6 +23,57 @@
 class Ddth_Adodb_AdodbHelper {
 
     /**
+     * Static cache used by function {@link loadSqlStatement()}.
+     *
+     * @var Array
+     */
+    private static $sqlStatementCache = Array();
+
+    /**
+     * Loads a SQL statement from a file ({@link Ddth_Commons_Properties .properties format}).
+     *
+     * The file contains SQL statement in the following format:
+     * <code>
+     * # Each line is a sql statement, in .properties format
+     * <name>=<the SQL query>
+     *
+     * # Example:
+     * sql.selectUserById = SELECT * FROM tbl_user WHERE id=?
+     * sql.deleteUserByEmail = DELETE FROM tbl_user WHERE email=?
+     * sql.createUser = INSERT INTO tbl_user (id, username, email) VALUES (?, ?, ?)
+     * </code>
+     *
+     * Usage example:
+     * <code>
+     * //obtain an ADOConnection
+     * $adoConn = ...;
+     *
+     * $configFile = 'dphp-adodb.sql.properties';
+     * $sql = Ddth_Adodb_AdodbHelper::loadSqlStatement($configFile, 'sql.selectYserById');
+     * $values = Array(1);
+     * $resultSet = $adoConn->Execute($sql, $values);
+     * </code>
+     *
+     * @param string $configFile name of the configuration file
+     * @param string $name name of the SQL statement to load. If NULL is supplied,
+     * the function returns all the SQL statements as a {@link Ddth_Commons_Properties} object
+     * @since function available since v0.2
+     */
+    public static function loadSqlStatement($configFile, $name = NULL) {
+        $props = isset(self::$sqlStatementCache[$configFile]) ? self::$sqlStatementCache[$configFile] : NULL;
+        if ($props === NULL) {
+            $fileContent = Ddth_Commons_Loader::loadFileContent($configFile);
+            if ($fileContent === NULL || $fileContent === "") {
+                return NULL;
+            }
+            $props = new Ddth_Commons_Properties();
+            $props->import($fileContent);
+            self::$sqlStatementCache[$configFile] = $props;
+        }
+        return $name !== NULL ? $props->getProperty($name) : $props;
+    }
+
+    /**
      * A shortcut to to build string with n question marks separated by commas (e.g. "?,?,?,?").
      *
      * @param int
@@ -32,11 +81,11 @@ class Ddth_Adodb_AdodbHelper {
      */
     public static function buildArrayParams($count = 1) {
         $count += 0;
-        if ( $count < 1 ) {
+        if ($count < 1) {
             return '';
         }
         $result = '?';
-        for ( $i = 1; $i < $count; $i++ ) {
+        for ($i = 1; $i < $count; $i++) {
             $result .= ',?';
         }
         return $result;
@@ -52,7 +101,7 @@ class Ddth_Adodb_AdodbHelper {
      * @since function available since v0.1.5.1
      */
     public static function prepareSql($conn, $sql, $params = Array()) {
-        foreach ( $params as $p ) {
+        foreach ($params as $p) {
             $sql = str_replace('${' . $p . '}', $conn->Param($p), $sql);
         }
         return $sql;
