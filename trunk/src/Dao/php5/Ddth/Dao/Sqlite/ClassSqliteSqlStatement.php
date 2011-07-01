91a2deb9e7f1a2f1e23032f7e08164408c11da87
btnguyen2k@gmail.com <btnguyen2k@gmail.com@0bc26c09-3644-0410-ad72-b9ecc7d9cb2a>
2011-07-01 21:31:30 +0000

    * Fixed a bug in "escape" function when escaping value 0.

--- trunk/src/Dao/php5/Ddth/Dao/Sqlite/ClassSqliteSqlStatement.php
+++ trunk/src/Dao/php5/Ddth/Dao/Sqlite/ClassSqliteSqlStatement.php
@@ -27,7 +27,7 @@ class Ddth_Dao_Sqlite_SqliteSqlStatement extends Ddth_Dao_SqlStatement {
      * @see Ddth_Dao_SqlStatement::escape()
      */
     protected function escape($conn, $value) {
-        return $value != NULL ? sqlite_escape_string($value) : NULL;
+        return $value !== NULL ? sqlite_escape_string($value) : NULL;
     }
 
     /**
