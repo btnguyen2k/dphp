7a471340eab6432402d09fa520c1eef1b9907fce
btnguyen2k <btnguyen2k@gmail.com>
2012-03-20 14:01:00 +0000

    * Added configuration to DAO

--- src/Dao/php5/Ddth/Dao/ClassIDao.php
+++ src/Dao/php5/Ddth/Dao/ClassIDao.php
@@ -7,39 +7,45 @@
  *
  * COPYRIGHT: See the included copyright.txt file for detail.
  *
- * @package     Dao
- * @author      Thanh Ba Nguyen <btnguyen2k@gmail.com>
- * @version     $Id$
- * @since       File available since v0.2
+ * @package Dao
+ * @author Thanh Ba Nguyen <btnguyen2k@gmail.com>
+ * @version $Id: ClassIDao.php 258 2010-12-28 10:39:48Z btnguyen2k@gmail.com $
+ * @since File available since v0.2
  */
 
 /**
  * This class represents a DAO (business object manager).
  *
- * @package     Dao
- * @author      Thanh Ba Nguyen <btnguyen2k@gmail.com>
- * @since      	Class available since v0.2
+ * @package Dao
+ * @author Thanh Ba Nguyen <btnguyen2k@gmail.com>
+ * @since Class available since v0.2
  */
 interface Ddth_Dao_IDao {
 
     /**
      * Gets a connection to persistent storage.
      *
-     * @param bool $startTransaction indicates that if a transaction is automatically started
-     * @return mixed the connection, or NULL if the connection can not be created
+     * @param bool $startTransaction
+     *            indicates that if a transaction is automatically started
+     * @return mixed the connection, or NULL if the connection can not be
+     *         created
      */
-    public function getConnection($startTransaction=FALSE);
+    public function getConnection($startTransaction = FALSE);
 
     /**
      * Closes the connection to persistent storage.
      *
-     * Normally this function closes the open connection only when necessary. Set $forceClose
+     * Normally this function closes the open connection only when necessary.
+     * Set $forceClose
      * to force the open connection to be closed.
      *
-     * @param bool $hasError indicates that an error has occurred during the usage of the connection
-     * @param bool $forceClose force the connection to be closed
+     * @param bool $hasError
+     *            indicates that an error has occurred during the usage of the
+     *            connection
+     * @param bool $forceClose
+     *            force the connection to be closed
      */
-    public function closeConnection($hasError=FALSE, $forceClose=FALSE);
+    public function closeConnection($hasError = FALSE, $forceClose = FALSE);
 
     /**
      * Gets the DAO factory instance.
@@ -52,7 +58,8 @@ interface Ddth_Dao_IDao {
      * Initializes the DAO.
      *
      * @param Ddth_Dao_IDaoFactory $daoFactory
+     * @param Array $config
      */
-    public function init($daoFactory);
+    public function init($daoFactory, $config = Array());
 }
 ?>
