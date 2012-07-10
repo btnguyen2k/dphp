c57229c9e337546961d7f3f17687b0131094dce1
btnguyen2k@gmail.com <btnguyen2k@gmail.com@0bc26c09-3644-0410-ad72-b9ecc7d9cb2a>
2012-07-10 20:08:50 +0000

    * Cache version 0.3.0     - Update Ddth_Cache_CacheEntry: support expireAfterWrite and expireAfterAccess

--- trunk/src/Cache/php5/Ddth/Cache/ClassCacheEntry.php
+++ trunk/src/Cache/php5/Ddth/Cache/ClassCacheEntry.php
@@ -7,21 +7,24 @@
  *
  * COPYRIGHT: See the included copyright.txt file for detail.
  *
- * @package     Cache
- * @author      Thanh Ba Nguyen <btnguyen2k@gmail.com>
- * @version     $Id$
- * @since       File available since v0.1
+ * @package Cache
+ * @author Thanh Ba Nguyen <btnguyen2k@gmail.com>
+ * @version $Id: ClassCacheEntry.php 251 2010-12-25 19:21:35Z
+ *          btnguyen2k@gmail.com $
+ * @since File available since v0.1
  */
 
 /**
  * A cache entry wrapper.
  *
- * This class wraps a cache entry inside and also provides extra functionality such as
+ * This class wraps a cache entry inside and also provides extra functionality
+ * such as
  * max idle time.
  *
  * Usage:
  * <code>
- * $cacheEntry = new Ddth_Cache_CacheEntry($realValue, 3600); //max idle time = 3600 seconds
+ * $cacheEntry = new Ddth_Cache_CacheEntry($realValue, 3600); //max idle time =
+ * 3600 seconds
  * //put $cacheEntry to cache
  * $cache->put($key, $cacheEntry);
  * //...
@@ -30,22 +33,43 @@
  * $value = $cacheEntry!==NULL ? $cacheEntry->getValue() : NULL;
  * </code>
  *
- * @package    	Cache
- * @author     	Thanh Ba Nguyen <btnguyen2k@gmail.com>
- * @since      	Class available since v0.1
+ * @package Cache
+ * @author Thanh Ba Nguyen <btnguyen2k@gmail.com>
+ * @since Class available since v0.1
  */
 class Ddth_Cache_CacheEntry {
     /**
+     *
      * @var mixed
      */
     private $value;
 
     /**
+     * Specifies that cache entry should expire once a fixed duration (in
+     * seconds) has
+     * elapsed after the entry's creation.
+     *
      * @var int
      */
-    private $maxIdleTime;
+    private $expireAfterWrite = -1;
 
     /**
+     * Specifies that cache entry should expire once a fixed duration (in
+     * seconds) has
+     * elapsed after the entry's creation, or its last access.
+     *
+     * @var int
+     */
+    private $expireAfterAccess = -1;
+
+    /**
+     *
+     * @var int
+     */
+    private $creationTimestamp;
+
+    /**
+     *
      * @var int
      */
     private $lastAccessTimestamp;
@@ -54,30 +78,18 @@ class Ddth_Cache_CacheEntry {
      * Constructs a new Ddth_Cache_CacheEntry object
      *
      * @param mixed $value
-     * @param int $maxIdleTime max idle time (in seconds)
+     * @param int $expireAfterWrite
+     *            (in seconds)
+     * @param int $expireAfterAccess
+     *            (in seconds)
      */
-    public function __construct($value, $maxIdleTime=0) {
+    public function __construct($value, $expireAfterWrite = -1, $expireAfterAccess = -1) {
         $this->value = $value;
-        $this->maxIdleTime = $maxIdleTime+0;
-        $this->lastAccessTimestamp = time();
-    }
-
-    /**
-     * Gets max idle time.
-     *
-     * @return int max idle time (in seconds)
-     */
-    public function getMaxIdleTime() {
-        return $this->maxIdleTime;
-    }
-
-    /**
-     * Sets max idle time.
-     *
-     * @param int $maxIdleTime max idle time (in seconds)
-     */
-    public function setMaxIdleTime($maxIdleTime) {
-        $this->maxIdleTime = $maxIdleTime+0;
+        $this->expireAfterAccess = $expireAfterAccess;
+        $this->expireAfterWrite = $expireAfterWrite;
+        $timestamp = time();
+        $this->creationTimestamp = $timestamp;
+        $this->lastAccessTimestamp = $timestamp;
     }
 
     /**
@@ -86,12 +98,37 @@ class Ddth_Cache_CacheEntry {
      * @return mixed
      */
     public function getValue() {
-        if ( !$this->isExpired() ) {
+        if (!$this->isExpired()) {
             $this->lastAccessTimestamp = time();
             return $this->value;
         } else {
             return NULL;
         }
+    }
+
+    public function getExpireAfterWrite() {
+        return $this->expireAfterWrite;
+    }
+
+    public function setExpireAfterWrite($expireAfterWrite) {
+        $this->expireAfterWrite = $expireAfterWrite;
+    }
+
+    public function getExpireAfterAccess() {
+        return $this->expireAfterAccess;
+    }
+
+    public function setExpireAfterAccess($expireAfterAccess) {
+        $this->expireAfterAccess = $expireAfterAccess;
+    }
+
+    /**
+     * Gets entry's creation timestamp.
+     *
+     * @return int UNIX timestamp
+     */
+    public function getCreationTimestamp() {
+        return $this->creationTimestamp;
     }
 
     /**
@@ -109,8 +146,12 @@ class Ddth_Cache_CacheEntry {
      * @return bool
      */
     public function isExpired() {
-        $maxIdleTime = $this->maxIdleTime;
-        return $maxIdleTime > 0 && $this->lastAccessTimestamp+$maxIdleTime < time();
+        if ($this->expireAfterWrite >= 0) {
+            return $this->expireAfterWrite + $this->creationTimestamp < time();
+        }
+        if ($this->expireAfterAccess >= 0) {
+            return $this->expireAfterAccess + $this->lastAccessTimestamp < time();
+        }
+        return TRUE;
     }
 }
-?>
